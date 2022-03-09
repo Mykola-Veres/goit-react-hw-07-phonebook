4d30b1ed6ec5b24dadb5273f04ffe378a8c25ca9
Mykola-Veres <veresnickolay@gmail.com>
2022-03-09 21:54:39 +0200

    update

--- src/redux/contactsAPI.js
+++ src/redux/contactsAPI.js
@@ -1,7 +1,5 @@
-// Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// Define a service using a base URL and expected endpoints
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2' }),
